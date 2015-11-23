00b76cc71616b5cff2c83f84f4b64a3aa78a0694
Nathan Cooper <nathan.cooper.nzl@gmail.com>
2015-11-23 20:53:14 +1300

    Setup chat system using signalR, updated css file with style factors for the chat area, and added link to chat window to other pages

---
+++ Web App/University_of_Contoso/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(University_of_Contoso.Startup))]
+
+namespace University_of_Contoso
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
